cf60f042565eb2b1816bae1f6a2a553f73262a0a
Matias Senger <matias.senger@cern.ch>
2023-04-06 07:04:04 +0200

    Added init and config parameters

--- user/CAEN_DT5742/python/CAENDT5742Producer.py
+++ user/CAEN_DT5742/python/CAENDT5742Producer.py
@@ -24,26 +24,56 @@ class CAENDT5742Producer(pyeudaq.Producer):
 
 	@exception_handler
 	def DoInitialise(self):
-		self._digitizer = CAEN_DT5742_Digitizer(0) # Should I do this here?
+		LinkNum = int(self.GetInitItem('LinkNum'))
+		if not hasattr(self, '_digitizer'):
+			self._digitizer = CAEN_DT5742_Digitizer(LinkNum=LinkNum)
 		EUDAQ_INFO(f'CAENDT5742Producer: DoInitialise, connected with {self._digitizer.idn}')
 
 	@exception_handler
 	def DoConfigure(self):
+		CONFIGURE_PARAMS = {
+			'sampling_frequency_MHz': dict(
+				set_method = 'set_sampling_frequency',
+				default = 5000,
+				type = int,
+			),
+			'max_num_events_BLT': dict(
+				set_method = 'set_max_num_events_BLT',
+				default = 1,
+				type = int,
+			),
+			'fast_trigger_threshold_ADCu': dict(
+				set_method = 'set_fast_trigger_threshold',
+				type = int,
+			),
+			'post_trigger_size': dict(
+				set_method = 'set_post_trigger_size',
+				default = 1,
+				type = int,
+			),
+		}
+		
 		EUDAQ_INFO('CAENDT5742Producer: DoConfigure')
 		self._digitizer.reset()
-		self._digitizer.set_sampling_frequency(MHz=5000)
+		
+		for param_name in CONFIGURE_PARAMS:
+			_ = self.GetConfigItem(param_name)
+			CONFIGURE_PARAMS[param_name]['value'] = CONFIGURE_PARAMS[param_name]['type'](_) if _ != '' else CONFIGURE_PARAMS[param_name].get('default')
+			param_value = CONFIGURE_PARAMS[param_name]['value']
+			if param_value is None: # This means it is mandatory and was not specified, i.e. there is no default value.
+				raise RuntimeError(f'Configuration parameter {repr(param_name)} is mandatory and was not specified in the configuration file. ')
+			getattr(self._digitizer, CONFIGURE_PARAMS[param_name]['set_method'])(param_value)
+			EUDAQ_INFO(f'CAENDT5742Producer: {repr(param_name)} was configured.')
+		
 		self._digitizer.set_record_length(1024)
-		self._digitizer.set_max_num_events_BLT(1) # Maximum number of events per block transfer.
 		self._digitizer.set_acquisition_mode('sw_controlled')
 		self._digitizer.set_ext_trigger_input_mode('disabled')
 		self._digitizer.set_fast_trigger_mode(enabled=True)
 		self._digitizer.set_fast_trigger_digitizing(enabled=True)
 		self._digitizer.enable_channels(group_1=True, group_2=False)
 		self._digitizer.set_fast_trigger_DC_offset(V=0)
-		self._digitizer.set_post_trigger_size(1)
 		for ch in [0,1]:
 			self._digitizer.set_trigger_polarity(channel=ch, edge='rising')
-		self._digitizer.set_fast_trigger_threshold(26340)
 
 	@exception_handler
 	def DoStartRun(self):
@@ -86,8 +116,9 @@ if __name__ == "__main__":
 	import time
 	
 	myproducer = CAENDT5742Producer("CAEN_digitizer", "tcp://localhost:44000")
-	print ("connecting to runcontrol in localhost:44000", )
+	print ("Connecting to runcontrol in localhost:44000...")
 	myproducer.Connect()
 	time.sleep(2)
+	print('Ready!')
 	while(myproducer.IsConnected()):
 		time.sleep(1)
